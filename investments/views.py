4191d1b87c6ee4136b7a37d6352391c6d1b8daf3
harrry <chegeharrison10@gmail.com>
2024-09-19 13:00:47 +0300

    feat: implement viewsets for InvestmentAccount and Transaction models with custom permissions

--- investments/views.py
+++ investments/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import InvestmentAccount, Transaction, AccountMembership
+from .serializers import InvestmentAccountSerializer, TransactionSerializer
+from .permissions import CanViewAccount, CanManageAccount, CanPostTransaction, IsAccountOwner
+
 
 # Create your views here.
+class InvestmentAccountViewSet(viewsets.ModelViewSet):
+    queryset = InvestmentAccount.objects.all()
+    serializer_class = InvestmentAccountSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.IsAuthenticated(), CanViewAccount()]
+        elif self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated(), CanManageAccount()]
+        return [permissions.IsAuthenticated()]
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.IsAuthenticated(), CanPostTransaction()]
+        elif self.action in ['list', 'retrieve']:
+            return [permissions.IsAuthenticated(), IsAccountOwner()]
+        return [permissions.IsAuthenticated()]
+    
+    def perform_create(self, serializer):
+        account = serializer.validated_data['account']
+        if not account.accountmembership_set.filter(user=self.request.user, permission='post').exists():
+            raise permissions.PermissionDenied('You do not have permission to post transactions to this account.')
+        serializer.save(user=self.request.user)
